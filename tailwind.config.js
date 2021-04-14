834c4ba440532ab453e088f095264ce8155207a1
RubinVelcani <rubin.velcani@gmail.com>
2021-04-14 17:34:21 +0200

    '0js and custom css'

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,6 @@ module.exports = {
     boxShadow: {
       'header-bottom': '1px 2px 13px 0px rgba(0, 0, 0, 0.25)'
     },
-    maxWidth: {
-      'laptop': '1024px'
-    },
-    width: {
-      'laptop-w': '1024px'
-    },
     extend: {},
   },
   variants: {
