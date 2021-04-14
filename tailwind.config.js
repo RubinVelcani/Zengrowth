c0fd4a8a54fd0fce7886fc01539aae92b4ac053b
RubinVelcani <rubin.velcani@gmail.com>
2021-04-14 16:09:21 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    boxShadow: {
+      'header-bottom': '1px 2px 13px 0px rgba(0, 0, 0, 0.25)'
+    },
+    maxWidth: {
+      'laptop': '1024px'
+    },
+    width: {
+      'laptop-w': '1024px'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
